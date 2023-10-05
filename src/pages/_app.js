08d0de0c6fd098668e91942da95bb7770a4590cd
Aaron Keohane <aakeohane@gmail.com>
2023-10-04 18:30:45 -0700

    vercel build test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import Layout from '../components/layout';
 import { Inter, Bebas_Neue } from 'next/font/google'
 import localFont from 'next/font/local'
-import Image from 'next/image';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -16,12 +15,13 @@ const bebasNeue = Bebas_Neue({
   display: 'swap',
 })
 
-const blastimo = localFont({
-  src: '../../public/fonts/Blastimo.ttf',
-  display: 'swap'
-})
 const blowbrush = localFont({
   src: '../../public/fonts/blowbrush.otf',
+  display: 'swap'
+})
+
+const blastimo = localFont({
+  src: '../../public/fonts/Blastimo.ttf',
   display: 'swap'
 })
 
@@ -39,13 +39,6 @@ export const bebasNeueFontClass = bebasNeue.className
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      {/* <Image
-        alt="background"
-        layout='fill'
-        objectFit='cover'
-        sizes="100vw"
-        src="/images/watercolor-paper-background-2.jpg"
-      /> */}
       <Component {...pageProps} />
     </Layout>
   );
