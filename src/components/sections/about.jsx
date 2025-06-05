aa263b463be948e596847d84e038ab7e7167ac80
Aaron <aakeohane@gmail.com>
2025-06-04 23:50:13 -0700

    responsiveness for portrait picture as you scale larger screens

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -2,39 +2,39 @@ import styles from './about.module.css'
 import Image from 'next/image';
 import profilePic from 'public/images/professional-portrait-v3-watercolor-v3.png'
 
-import { bebasNeueFontClass } from "@/app/layout"
-
 const About = () => {
 
   return (
-    <section id="about" data-section className={"about-container"}>
-            <h1>How did I know I was destined for coding?</h1>
-      <div className={styles["portrait-container"]}>
-          <div className={styles["profile-container"]}>
-            <Image
-              alt="personal logo" 
-              src={profilePic}
-              className={styles["portrait"]}
-              priority={true}
-              id="portrait"
-              fill={true}
-            />
-            <div id="drop1" className={styles["drop"]}></div>
-            <div className={styles["drop"]}></div>
-            <div className={styles["drop"]}></div>
+    <section id="about" data-section>
+      <div className={styles["about-container"]}>
+        <div className={styles["portrait-container"]}>
+              <Image
+                alt="personal logo" 
+                src={profilePic}
+                className={styles["portrait"]}
+                priority={true}
+                id="portrait"
+                fill={true}
+              />
+                <div id="drop1" className={styles["drop"]}></div>
+                <div className={styles["drop"]}></div>
+                <div className={styles["drop"]}></div>
+        </div>
+        <div className={styles["content-container"]}>
+          <h1 className={styles["title"]}>How did I know I was destined for coding?</h1>
+          <div>
+            I am always the first to google the moment someone asks a question.
+            Besides my googling talent, my academic background in the sciences 
+            has prepared me to problem solve, critically evaluate, and think 
+            imaginatively when facing technical challenges. Thankfully, my seemingly 
+            perpetual need to know is the perfect characteristic for someone in the 
+            tech field and I am ready to keep on learning! I aim to inspire positive 
+            environmental change through my work. I love the ocean, typography and 
+            living a zero waste lifestyle. When I'm not obsessing about responsive 
+            design, you can find me on the tennis courts. Take a peak at my CV below.
           </div>
+        </div>
       </div>
-      <p className={`${bebasNeueFontClass}`}>
-        I am always the first to google the moment someone asks a question.
-        Besides my googling talent, my academic background in the sciences 
-        has prepared me to problem solve, critically evaluate, and think 
-        imaginatively when facing technical challenges. Thankfully, my seemingly 
-        perpetual need to know is the perfect characteristic for someone in the 
-        tech field and I am ready to keep on learning! I aim to inspire positive 
-        environmental change through my work. I love the ocean, typography and 
-        living a zero waste lifestyle. When I'm not obsessing about responsive 
-        design, you can find me on the tennis courts. Take a peak at my CV below.
-      </p>
     </section>
   )
 }
