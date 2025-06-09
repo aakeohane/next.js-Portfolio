49b1c84b42757022282b3f70f9d805238b9f6a9f
Aaron <aakeohane@gmail.com>
2025-06-09 11:41:27 -0700

    flex alignment, info and content for contact me page

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './contact.module.css'
 import Image from "next/image";
 
@@ -6,55 +7,67 @@ const Contact = () => {
   
   return (
     <section id="contact" className={styles["contact-container"]} data-section>
-      <h1>
+      <h1 className={styles["header"]}>
         Contact
       </h1>
-      <p className={styles["first-blurb"]}>
-       I am available for full-time employment, collaboration and freelance work.
-      </p>
-      <div className={styles["social-icons-container"]}>
-          <a
-            href="https://twitter.com/Aakeocaine"
-            aria-label="Twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image 
-              className={styles["twitter-icon"]}  
-              src='/images/svg/twitter.svg'
-              width={socialIconSize}
-              height={socialIconSize}
-              alt="Twitter Logo"
-            />
-          </a>
-          <a
-            href="https://github.com/aakeohane"
-            aria-label="GitHub"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image 
-              className={styles["github-icon"]}  
-              src='/images/svg/github.svg'
-              width={socialIconSize}
-              height={socialIconSize}
-              alt="Github Logo"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/aaron-keohane-47112430/"
-            aria-label="LinkedIn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image 
-              className={styles["linkedin-icon"]}  
-              src='/images/svg/linkedin.svg'
-              width={socialIconSize}
-              height={socialIconSize}
-              alt="LinkedIn Logo"
-            />
-          </a>
+      <div className={styles["main-content"]}>
+        <p>
+        I am available for full-time employment, collaboration and freelance work.
+        </p>
+        <h4>Aaron Keohane</h4>
+        <p>San Diego, California ☀️</p>
+        <button className={styles["resume-button"]}>
+            <Link                       
+              href="files/keohane-resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >Resume
+            </Link>
+        </button>
+        <div className={styles["social-icons-container"]}>
+            <a
+              href="https://twitter.com/Aakeocaine"
+              aria-label="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image 
+                className={styles["twitter-icon"]}  
+                src='/images/svg/twitter.svg'
+                width={socialIconSize}
+                height={socialIconSize}
+                alt="Twitter Logo"
+              />
+            </a>
+            <a
+              href="https://github.com/aakeohane"
+              aria-label="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image 
+                className={styles["github-icon"]}  
+                src='/images/svg/github.svg'
+                width={socialIconSize}
+                height={socialIconSize}
+                alt="Github Logo"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/aaron-keohane-47112430/"
+              aria-label="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image 
+                className={styles["linkedin-icon"]}  
+                src='/images/svg/linkedin.svg'
+                width={socialIconSize}
+                height={socialIconSize}
+                alt="LinkedIn Logo"
+              />
+            </a>
+          </div>
         </div>
     </section>
   )
