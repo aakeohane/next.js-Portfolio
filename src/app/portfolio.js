8b667d26040b5ed2afb6b2d55366af921686ee5b
Aaron <aakeohane@gmail.com>
2025-08-28 12:32:16 -0700

    added gsap to jelly

--- src/app/portfolio.js
+++ src/app/portfolio.js
@@ -10,50 +10,61 @@ import Header from '@/components/header'
 
 import watercolorJelly from 'public/images/jelly.png'
 import Image from 'next/image';
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
 
 import styles from './portfolio.module.css'
 
 const Portfolio = ({allWerkData}) => {
-  
 
   const size = useWindowSize();
   const [isLoading, setIsLoading] = useState(true)
 
   function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
+    // Initialize state with undefined width/height so server and client renders match
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
 
-  useEffect(() => {
-    if (isLoading) {
-      setIsLoading(false)
-    }
-    
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+    useEffect(() => {
+      if (isLoading) {
+        setIsLoading(false)
+      }
       
-    }
-    
-    window.addEventListener("resize", handleResize);
-     
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    
-    // Removes event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
 
-  return windowSize;
-}
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+        
+      }
+      
+      window.addEventListener("resize", handleResize);
+      
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      
+      // Removes event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+
+    return windowSize;
+  }
+
+  useGSAP(() => {
+        
+    gsap.from("#jelly", {
+      opacity: 0,
+      translateX: [40],
+      duration: 2,
+      delay: 2
+    })
+  }, [!isLoading])
 
   return (
     <>
